26242be22a54aee831af63b1b8b4a27c3aa889d7
Bemoliph <bemoliph@gmail.com>
2015-09-24 22:42:00 -0700

    GameWorld now handles all entities; removed MushroomField

--- core/src/info/miningyour/games/centipede/game/GameWorld.java
+++ core/src/info/miningyour/games/centipede/game/GameWorld.java
@@ -2,34 +2,100 @@ package info.miningyour.games.centipede.game;
 
 import com.badlogic.gdx.math.Rectangle;
 import info.miningyour.games.centipede.utils.QuadTree;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class GameWorld {
 
     private Rectangle bounds;
 
+    private Random rng;
+    private List<GameObject> gameObjects;
     private Collider collider;
-    private MushroomField mushField;
+
     private Player player;
     private Bullet bullet;
+
+    private int mushroomCount;
+    private static final int mushroomSize = 8;
 
     public GameWorld(Rectangle bounds) {
         this.bounds = bounds;
 
+        rng = new Random();
+        gameObjects = new ArrayList<GameObject>();
         collider = new Collider(bounds);
-        mushField = new MushroomField(30, 30);
+
+        spawnPlayer(16, 16);
+
+        mushroomCount = 0;
+        populateMushrooms(45 - rng.nextInt(5));
+    }
+
+    private void spawnPlayer(float x, float y) {
         player = new Player(16, 16);
+
+        gameObjects.add(player);
+        collider.add(player);
+
+        spawnBullet(player);
+    }
+
+    private void spawnBullet(Player player) {
         bullet = new Bullet(player);
 
-        collider.add(player);
+        gameObjects.add(bullet);
         collider.add(bullet);
-        Iterator<Mushroom> mushIter = mushField.getMushrooms();
-        while (mushIter.hasNext()) {
-            Mushroom mushroom = mushIter.next();
-            if (mushroom != null) {
-                collider.add(mushroom);
+    }
+
+    private boolean isMushroomAt(float x, float y) {
+        for (GameObject obj : gameObjects) {
+            if (obj instanceof Mushroom) {
+                Mushroom mushroom = (Mushroom) obj;
+
+                if (mushroom.getX() == x && mushroom.getY() == y) {
+                    return true;
+                }
             }
         }
+
+        return false;
+    }
+
+    public void spawnMushroom(float x, float y) {
+        Mushroom mushroom = new Mushroom(x, y);
+
+        gameObjects.add(mushroom);
+        collider.add(mushroom);
+        mushroomCount++;
+    }
+
+    private void populateMushrooms(int populationCount) {
+        while (mushroomCount < populationCount) {
+            int x = mushroomSize * rng.nextInt((int) bounds.width / mushroomSize);
+            int y = mushroomSize * (rng.nextInt((int) bounds.height / mushroomSize - 4) + 4);
+
+            if (!isMushroomAt(x, y)) {
+                spawnMushroom(x, y);
+            }
+        }
+    }
+
+    public void spawnFlea(float x, float y) {
+
+    }
+
+    public void spawnSpider(float x, float y) {
+
+    }
+
+    public void spawnCentipede(float x, float y, int segments) {
+
+    }
+
+    public void spawnScorpion(float x, float y) {
+
     }
 
     public Player getPlayer() {
